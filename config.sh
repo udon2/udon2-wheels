d93f08df06b2aa97a20fe8a756606d5648b2fd9b
Dmytro Kalpakchi <dkalpackchi@gmail.com>
2022-02-01 15:27:55 +0100

    Try OSX build [skip appveyor]

--- config.sh
+++ config.sh
@@ -1,15 +1,24 @@
 function pre_build {
-    yum install -y wget
-    yum install -y ca-certificates
-    wget https://boostorg.jfrog.io/artifactory/main/release/1.74.0/source/boost_1_74_0.tar.bz2
-    tar --bzip2 -xf boost_1_74_0.tar.bz2
-    cp -r "boost-python-manylinux2010-$PLAT/" boost
-    mkdir boost/include
-    mv boost_1_74_0/boost boost/include
-    ls boost
-    pwd
-    export BOOST_DIR="`pwd`/boost"
-    export LD_LIBRARY_PATH="$LD_LIBRARY_PATH:`pwd`/boost/lib"
+    if [ -z "$IS_OSX" ]; then
+        brew install boost --with-python
+        brew install boost-python
+        brew list | grep 'boost'
+        brew info boost-python
+        export BOOST_DIR="`pwd`/boost"
+        export LD_LIBRARY_PATH="$LD_LIBRARY_PATH:`pwd`/boost/lib"
+    else
+        yum install -y wget
+        yum install -y ca-certificates
+        wget https://boostorg.jfrog.io/artifactory/main/release/1.74.0/source/boost_1_74_0.tar.bz2
+        tar --bzip2 -xf boost_1_74_0.tar.bz2
+        cp -r "boost-python-manylinux2010-$PLAT/" boost
+        mkdir boost/include
+        mv boost_1_74_0/boost boost/include
+        ls boost
+        pwd
+        export BOOST_DIR="`pwd`/boost"
+        export LD_LIBRARY_PATH="$LD_LIBRARY_PATH:`pwd`/boost/lib"
+    fi
 }
 
 function run_tests {
